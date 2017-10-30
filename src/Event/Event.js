e591e2999b7841a782d099ba1d3ce11452acf67b
Luke Finney <lucaswfinney@gmail.com>
2017-10-29 20:17:43 -0600

    reformat date and time fixes #6

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -11,7 +11,9 @@ const Event = ({ comp }) => {
       <h3>{comp.compName}</h3>
       <h4>{comp.sport} - {comp.competitiveness}</h4>
       <h4>{comp.players}</h4>
-      <h4>{dateFormat(comp.date, 'mediumDate')}, {convertTime(comp.time)}, {comp.location}</h4>
+      <h4>{convertTime(comp.time) + ' '}
+        on {dateFormat(comp.date, 'mediumDate') + ' '}
+        at {comp.location}</h4>
       <p>{comp.details}</p>
       <div className="park-map"></div>
       <button>Count Me In!</button>
